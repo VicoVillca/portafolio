697c9673eb8b45df266782bd670a38a84577f373
VicoVillca <vicovillca@hotmail.com>
2023-02-11 23:50:05 -0400

    agregamos enlaces a los cards de proyectos

--- src/componentes/Proyectos.js
+++ src/componentes/Proyectos.js
@@ -1,65 +1,58 @@
-import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ListSubheader from '@mui/material/ListSubheader';
-import Box from '@mui/material/Box';
-import { useMediaQuery } from '@mui/material';
-import ImageItem from './ImageItem';
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ListSubheader from "@mui/material/ListSubheader";
+import Box from "@mui/material/Box";
+import { useMediaQuery } from "@mui/material";
+import ImageItem from "./ImageItem";
 // componentes de card
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import GitHubIcon from "@mui/icons-material/GitHub";
 //{"/*<ImageItem item={item} key={item.img}/>*/}
 //imagenes de proyectos
-import portafolio from './images/proyectos/portafolio.png';
-import obi from './images/proyectos/obi.png';
-import goldprice from './images/proyectos/golgprice.png';
+import portafolio from "./images/proyectos/portafolio.png";
+import obi from "./images/proyectos/obi.png";
+import goldprice from "./images/proyectos/golgprice.png";
 export default function Proyectos() {
-    const matches = useMediaQuery('(min-width:600px)');
+  const matches = useMediaQuery("(min-width:600px)");
   return (
-    <Box
-    sx={{ flexGrow: 5 }}
-    > 
-    <ListSubheader component="div">Proyectos</ListSubheader>
-    <ImageList  cols={matches ? 3 : 1} rowHeight={164} sx={{ pr: 5,pl: 5 }} variant="masonry" gap={8}>
-      {itemData.map((item) => (
-        
-        
-        <Card key={item.title} sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={item.img}
-          alt="green iguana"
-        />
-       
-      </CardActionArea>
-      <CardActions>
-        Portafolio
-        <IconButton
-             edge="start"
-             size="small"
-             color="primary"
-              >
+    <Box sx={{ flexGrow: 5 }}>
+      <ListSubheader component="div">Proyectos</ListSubheader>
+      <ImageList
+        cols={matches ? 3 : 1}
+        rowHeight={164}
+        sx={{ pr: 5, pl: 5 }}
+        variant="masonry"
+        gap={8}
+      >
+        {itemData.map((item) => (
+          <Card key={item.title} sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={item.img}
+                alt="green iguana"
+              />
+            </CardActionArea>
+            <CardActions>
+              {item.title}
+              <IconButton edge="start" size="small" color="primary"  onClick={() => window.open(item.demo)}>
                 <OpenInNewIcon />
               </IconButton>
-              <IconButton
-              size="small"
-              color="primary"
-              >
-                <GitHubIcon/>
+              <IconButton size="small" color="primary"  onClick={() => window.open(item.git)}>
+                <GitHubIcon />
               </IconButton>
-      </CardActions>
-    </Card>
-      ))}
-    </ImageList>
-   
+            </CardActions>
+          </Card>
+        ))}
+      </ImageList>
     </Box>
   );
 }
@@ -67,22 +60,20 @@ export default function Proyectos() {
 const itemData = [
   {
     img: portafolio,
-    title: 'Portafolio',
-    demo: 'https://vicovillca.github.io/portafolio/',
-    git: 'https://github.com/VicoVillca/portafolio'
+    title: "Portafolio",
+    demo: "https://vicovillca.github.io/portafolio/",
+    git: "https://github.com/VicoVillca/portafolio",
   },
   {
     img: obi,
-    title: 'ObiAdmin',
-    demo: 'https://obi-font-end.vercel.app/',
-    git: 'https://github.com/VicoVillca/Obi-FontEnd'
+    title: "ObiAdmin",
+    demo: "https://obi-font-end.vercel.app/",
+    git: "https://github.com/VicoVillca/Obi-FontEnd",
   },
   {
     img: goldprice,
-    title: 'GoldPrice',
-    demo: 'https://vicovillca.github.io/GoldPrice',
-    git: 'https://github.com/VicoVillca/Obi-FontEnd'
+    title: "GoldPrice",
+    demo: "https://vicovillca.github.io/GoldPrice",
+    git: "https://github.com/VicoVillca/Obi-FontEnd",
   },
-  
 ];
-
